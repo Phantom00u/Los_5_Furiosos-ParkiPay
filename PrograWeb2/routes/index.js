25873eeb8c8662b7063003b78aceb2237784758b
cheessyTaco <82736948+cheessyTaco@users.noreply.github.com>
2022-09-24 00:06:49 -0500

    a ver chirinos muevele ya

--- PrograWeb2/routes/index.js
+++ PrograWeb2/routes/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 //const UserRouter = require('./login.router');
 const loginService = require('./login.router');
 const registerService = require('./register.router');
-const UserProfileRouter = require('./UserProfile.router');
+const reserveService = require('./reservar.router');
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/api', router);
   router.use('/login', loginService);
   router.use('/signin', registerService);
-  router.use('/UserProfile', UserProfileRouter);
+  router.use('/reserve', reserveService);
   //router.use('/users', productsRouter);
   //router.use('/categories', productsRouter);
 }
